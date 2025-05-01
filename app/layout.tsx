378edf379bbfaa070f29bb4d8ee00e5158827cdf
Santosh Kalidindi <skalidindi8@gmail.com>
2025-04-30 23:22:30 -0700

    add speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 
 import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Roboto, Roboto_Mono } from "next/font/google";
 
 import "./globals.css";
@@ -32,6 +33,7 @@ export default function RootLayout({
       >
         {children}
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
